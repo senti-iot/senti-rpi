d86c692ba00f3548df33b20627abf9c2b4c9f140
Christian Broberg <cb@cb-pro.local>
2018-11-15 02:17:27 +0100

    Added a bunch of new modules

--- index.js
+++ index.js
@@ -13,9 +13,23 @@ const rpiDetect = require('./lib/rpi-detect')
 const rpiTemp = require('./lib/rpi-temp')
 const rpiSerial = require('./lib/rpi-serial')
 const rpiVersion = require('./lib/rpi-version')
+const rpiCurrentTimeAndHost = require('./lib/rpi-curtimehost')
+const rpiGPUTemp = require('./lib/rpi-gputemp')
+const rpiIP = require('./lib/rpi-ip')
+const rpiMemoryTotal = require('./lib/rpi-memorytotal')
+const rpiMemoryFree = require('./lib/rpi-memoryfree')
+const rpiMemoryAvailable = require('./lib/rpi-memoryavailable')
+const rpiMemoryUsage = require('./lib/rpi-memoryusage')
 
 // Expose senti-rpi modules
 module.exports.rpiDetect = rpiDetect
 module.exports.rpiTemp = rpiTemp
 module.exports.rpiSerial = rpiSerial
 module.exports.rpiVersion = rpiVersion
+module.exports.rpiCurrentTimeAndHost = rpiCurrentTimeAndHost
+module.exports.rpiGPUTemp = rpiGPUTemp
+module.exports.rpiIP = rpiIP
+module.exports.rpiMemoryTotal = rpiMemoryTotal
+module.exports.rpiMemoryFree = rpiMemoryFree
+module.exports.rpiMemoryAvailable = rpiMemoryAvailable
+module.exports.rpiMemoryUsage = rpiMemoryUsage
